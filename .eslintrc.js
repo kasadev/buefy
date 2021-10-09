2a53618fcb79323e56a220d56f97b10fb19397f1
laszlo-kasa <laci@kasa.com>
2021-10-09 16:22:53 +0200

    update config 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,74 @@
 
 module.exports = {
     env: {
-        jest: true
+        jest: true,
+        browser: true
+    },
+    parserOptions: {
+        ecmaVersion: 2020
     },
     extends: [
+        'standard',
         'plugin:vue/vue3-essential'
     ],
     rules: {
-        'max-len': ['warn', 150],
-        'vue/no-reserved-keys': ['warn'],
+        // eslint
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'space-before-function-paren': ['error', {
+            'anonymous': 'always',
+            'named': 'never'
+        }],
+        'max-len': ['error', {
+            'code': 150,
+            'tabWidth': 4,
+            'ignoreUrls': true,
+            'ignoreStrings': true,
+            'ignoreTemplateLiterals': true,
+            'ignoreRegExpLiterals': true
+        }],
+        'no-console': ['warn'],
+        'arrow-parens': ['error', 'always'],
+        'max-depth': ['error', 4],
+        'vue/html-indent': ['error', 4],
+        'vue/max-attributes-per-line': [2, {
+            'singleline': 2,
+            'multiline': {
+                'max': 1,
+                'allowFirstLine': false
+            }
+        }],
+
+        // vue
+        'vue/require-v-for-key': ['warn'],
+        'vue/order-in-components': ['error', {
+            order: [
+                'el',
+                'name',
+                'parent',
+                'functional',
+                ['delimiters', 'comments'],
+                ['components', 'directives', 'filters'],
+                'extends',
+                'mixins',
+                'inheritAttrs',
+                'model',
+                ['props', 'propsData'],
+                'data',
+                'computed',
+                'watch',
+                'methods',
+                'LIFECYCLE_HOOKS',
+                ['template', 'render'],
+                'renderError'
+            ]
+        }],
+        'vue/no-reserved-keys': 0,
+        'vue/require-default-prop': 0,
+
+        // import
+        'import/no-webpack-loader-syntax': 0,
         'vue/attributes-order': 'off',
         'vue/html-self-closing': ['error', {
             'html': {
--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,9 @@
-import { configureCompat } from 'vue'
+import { configureCompat } from '@vue/compat'
 import { config } from '@vue/test-utils'
 
 configureCompat({
+    MODE: 2,
     INSTANCE_LISTENERS: false, // https://github.com/vuejs/vue-next/issues/4566#issuecomment-917997056
+    ATTR_FALSE_VALUE: false
 })
 config.renderStubDefaultSlot = true
