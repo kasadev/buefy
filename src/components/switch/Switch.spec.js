0828a026fe5707daacad605324ac3dc109f0d257
laszlo-kasa <laci@kasa.com>
2021-10-11 14:05:24 +0200

    fix switch

--- src/components/switch/Switch.spec.js
+++ src/components/switch/Switch.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import { flushPromises, shallowMount } from '@vue/test-utils'
 import BSwitch from '@components/switch/Switch'
 
 let wrapper
@@ -14,61 +14,51 @@ describe('BSwitch', () => {
         })
     })
 
-    it('is called', () => {
-        expect(wrapper.name()).toBe('BSwitch')
-        expect(wrapper.isVueInstance()).toBeTruthy()
-    })
-
-    it('render correctly', () => {
-        expect(wrapper.html()).toMatchSnapshot()
-    })
-
     it('renders input element by default', () => {
-        expect(wrapper.contains('input')).toBeTruthy()
+        expect(wrapper.find('input')).toBeTruthy()
         expect(wrapper.classes()).toContain('switch')
     })
 
-    it('updates internal value when v-model is changed', () => {
+    it('updates internal value when v-model is changed', async () => {
         const newValue = 'switch value'
-        wrapper.setProps({ value: newValue })
+        await wrapper.setProps({ value: newValue })
         expect(wrapper.vm.newValue).toBe(newValue)
     })
 
-    it('emit input event when computedValue is set', () => {
+    it('emit input event when computedValue is set', async () => {
         const newValue = 'switch value'
         wrapper.vm.computedValue = newValue
+        await wrapper.vm.$nextTick()
         const valueEmitted = wrapper.emitted()['input'][0]
         expect(wrapper.vm.newValue).toBe(newValue)
         expect(valueEmitted).toContainEqual(newValue)
     })
 
-    it('method focus() gives focus to the input element', (done) => {
+    it('method focus() gives focus to the input element', async () => {
         wrapper.vm.$refs.input.focus = jest.fn()
         wrapper.vm.focus()
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.$refs.input.focus).toHaveBeenCalled()
-            done()
-        })
+        await flushPromises()
+        expect(wrapper.vm.$refs.input.focus).toHaveBeenCalled()
     })
 
-    it('applies passiveType prop properly', () => {
+    it('applies passiveType prop properly', async () => {
         const passiveType = 'is-danger'
         const value = false
-        wrapper.setProps({ passiveType, value })
+        await wrapper.setProps({ passiveType, value })
         const switchElement = wrapper.find('.check')
         expect(switchElement.classes()).toContain('is-danger-passive')
     })
 
-    it('does not have a label at left by default', () => {
+    it('does not have a label at left by default', async () => {
         const value = false
-        wrapper.setProps({ value })
+        await wrapper.setProps({ value })
         expect(wrapper.classes()).not.toContain('has-left-label')
     })
 
-    it('has label at left is left-label prop has been sent', () => {
+    it('has label at left is left-label prop has been sent', async () => {
         const leftLabel = true
         const value = false
-        wrapper.setProps({ leftLabel, value })
+        await wrapper.setProps({ leftLabel, value })
         expect(wrapper.classes()).toContain('has-left-label')
     })
 })
